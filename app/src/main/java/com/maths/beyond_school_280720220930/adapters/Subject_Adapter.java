10a05c7c3a1bf404751fcfe6ee61293ce596752c
Ayaan <aiyuayaan22@gmail.com>
2022-08-17 19:31:58 +0530

    Change : -  * ToolBar change

--- app/src/main/java/com/maths/beyond_school_280720220930/adapters/Subject_Adapter.java
+++ app/src/main/java/com/maths/beyond_school_280720220930/adapters/Subject_Adapter.java
@@ -6,13 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.maths.beyond_school_280720220930.AdditionActivity;
 import com.maths.beyond_school_280720220930.LearningActivity;
 import com.maths.beyond_school_280720220930.MainActivity;
 import com.maths.beyond_school_280720220930.R;
@@ -52,24 +50,23 @@ public class Subject_Adapter extends RecyclerView.Adapter<Subject_Adapter.Subjec
             public void onClick(View view) {
                 //needs to be intent values
                 if (res[0].toLowerCase(Locale.ROOT).equals("vocabulary")) {
-                    /*Intent intent = new Intent(context, EnglishActivity.class);
-                    context.startActivity(intent);*/
-                    Toast.makeText(context, res[1], Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(context, EnglishActivity.class);
+                    context.startActivity(intent);
+//                    Toast.makeText(context, res[1], Toast.LENGTH_SHORT).show();
                 } else {
 
 
-                    if (!res[0].equals("Multiplication")){
+                    if (!res[0].equals("Multiplication")) {
                         Intent intent = new Intent(context, LearningActivity.class);
                         intent.putExtra("subject", res[res.length - 1].toLowerCase());
                         intent.putExtra("max_digit", res[0]);
-                        intent.putExtra("video_url",subject_model.getUrl());
+                        intent.putExtra("video_url", subject_model.getUrl());
                         context.startActivity(intent);
-                    }
-                    else{
-                        Intent intent=new Intent(context, MainActivity.class);
+                    } else {
+                        Intent intent = new Intent(context, MainActivity.class);
                         intent.putExtra("subject", res[0].toLowerCase());
                         intent.putExtra("max_digit", res[3]);
-                        intent.putExtra("video_url",subject_model.getUrl());
+                        intent.putExtra("video_url", subject_model.getUrl());
                         context.startActivity(intent);
 
                     }
