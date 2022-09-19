5786320e0c9f25a0dedf13a4e431f9d4aa18e234
Abhisek <abhiseal45@gmail.com>
2022-09-19 12:24:46 +0530

    new_timer_added

--- app/src/main/java/com/maths/beyond_school_280720220930/AlarmAtTime.java
+++ app/src/main/java/com/maths/beyond_school_280720220930/AlarmAtTime.java
@@ -1,44 +1,60 @@
 package com.maths.beyond_school_280720220930;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.maths.beyond_school_280720220930.SP.PrefConfig;
 import com.maths.beyond_school_280720220930.databinding.ActivityAlarmAtTimeBinding;
 import com.maths.beyond_school_280720220930.model.AlarmReceiver;
 import com.maths.beyond_school_280720220930.utils.Utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+import java.util.TimeZone;
 
 public class AlarmAtTime extends AppCompatActivity {
-    private static final String SHARED_PREF_NAME = "Time";
-    private static final String KEY_HOUR = "hour";
-    private static final String KEY_MINUTE = "minute";
-    TextView titletext;
+    private String time = "";
 
-    Calendar calendar;
-    AlarmManager alarmManager;
-    PendingIntent pendingIntent;
-    TimePicker picker;
-    SwitchCompat setAlarm;
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
+
+    private int REQUEST_CALENDER_ACCESS = 100;
+    private TextView titletext;
+    private SwitchCompat setAlarm;
+    private String[] array;
+    private ArrayAdapter adapter;
+
 
     private ActivityAlarmAtTimeBinding binding;
 
@@ -51,189 +67,132 @@ public class AlarmAtTime extends AppCompatActivity {
         binding=ActivityAlarmAtTimeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        checkCalenderPermission();
 
         titletext = findViewById(R.id.titleText);
-
-        picker = (TimePicker) findViewById(R.id.datePicker1);
-        picker.setIs24HourView(false);
         setAlarm = findViewById(R.id.setAlarm);
-
-        createNotificationChannel();
         titletext.setText("Set Reminder");
-
-        //setting shared preferences
-        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        editor = sharedPreferences.edit();
-        if (sharedPreferences.contains(KEY_HOUR)) {
-            picker.setHour(sharedPreferences.getInt("hour", 12));
-            picker.setMinute(sharedPreferences.getInt("minute", 00));
-        }
         binding.toolBar.imageViewBack.setOnClickListener(view -> onBackPressed());
 
-        onPickerChange();
-        setAlarmButton();
+        setUpTextLayoutGrade();
+
+        time=PrefConfig.readIdInPref(getApplicationContext(),getResources().getString(R.string.timer_time));
+        if (!time.equals(""))
+            binding.textInputLayoutTimer.getEditText().setText(time);
+
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void onPickerChange() {
-        picker.setOnTimeChangedListener((timePicker, i, i1) -> {
-            if (setAlarm.isChecked()) {
-                cancelAlarm(1);
-                setAlarm(1);
-            }
-            putVal(timePicker.getHour(), timePicker.getMinute());
-        });
-    }
+
+    private void setUpTextLayoutGrade() {
+        array = getResources().getStringArray(R.array.time_slots);
+        adapter = new ArrayAdapter(this, R.layout.list_item, array);
+        AutoCompleteTextView editText = Objects.requireNonNull((AutoCompleteTextView) binding.textInputLayoutTimer.getEditText());
+        editText.setAdapter(adapter);
 
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void setAlarmButton() {
-        setAlarm.setChecked(PrefConfig.readBooleanInPref(this, getResources().getString(R.string.alarm_enable)));
-        setAlarm.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            if (isChecked) {
-                setAlarm(0);
-            } else {
-                cancelAlarm(0);
-            }
-            PrefConfig.writeBooleanInPref(this, isChecked, getResources().getString(R.string.alarm_enable));
-        });
-    }
-
-    /**
-     * @param request value when called from onPickerChange 1 for other 0
-     */
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void cancelAlarm(int request) {
-
-        Intent intent = new Intent(this, AlarmReceiver.class);
-//        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, Utils.getPendingIntentFlag());
-
-        if (alarmManager == null) {
-            alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-        }
-        alarmManager.cancel(pendingIntent);
-        if (request != 1)
-            Utils.simpleToast(this, "Reminder Cancel !!");
-//        picker.setHour(12);
-//        picker.setMinute(00);
-
-    }
-
-    /**
-     * @param request value when called from onPickerChange 1 for other 0
-     */
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void setAlarm(int request) {
-        try {
-            calendar = Calendar.getInstance();
-            if (Build.VERSION.SDK_INT >= 23) {
-                calendar.set(Calendar.HOUR_OF_DAY, picker.getHour());
-                calendar.set(Calendar.MINUTE, picker.getMinute());
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-                putVal(picker.getHour(), picker.getMinute());
-            } else {
-                calendar.set(Calendar.HOUR_OF_DAY, picker.getCurrentHour());
-                calendar.set(Calendar.MINUTE, picker.getCurrentMinute());
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-                putVal(picker.getCurrentHour(), picker.getCurrentMinute());
-            }
-            setAlarm(this, calendar, request);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e("Noti_Err", e.getMessage());
-            //    Toast.makeText(this, ""+e.toString(), Toast.LENGTH_SHORT).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            binding.kidsGrade.setText(binding.kidsGrade.getAdapter().getItem(0).toString(), false);
         }
     }
 
-
-    public void setAlarm(Context context, Calendar calendar, int request) {
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(context, AlarmReceiver.class);
-        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, Utils.getPendingIntentFlag());
-
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                AlarmManager.INTERVAL_DAY, pendingIntent);
-
-        if (request != 1)
-            Utils.simpleToast(this, "Reminder Set Successfully ");
-    }
-
-    private void putVal(int hour, int minute) {
-        editor.putInt("hour", hour);
-        editor.putInt("minute", minute);
-        editor.apply();
-    }
-
-    /*private void showTimePicker() {
-        timePicker=new MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_12H)
-                .setHour(12)
-                .setMinute(0)
-                .setTitleText("Select Alarm Time")
-                .build();
-        timePicker.show(getSupportFragmentManager(),"Beyond_school");
-        timePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.S)
-            @Override
-            public void onClick(View view) {
-                if (timePicker.getHour()>12){
-                    //selectedTime.setText(String.format("%02d",(timePicker.getHour()-12))+" : "+String.format("%02d",timePicker.getMinute())+" PM");
-
-                }else{
-                    //selectedTime.setText(timePicker.getHour()+" : "+timePicker.getMinute()+" AM");
-                }
-
-
-                calendar=Calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY,timePicker.getHour());
-                calendar.set(Calendar.MINUTE,timePicker.getMinute());
-                calendar.set(Calendar.SECOND,0);
-                calendar.set(Calendar.MILLISECOND,0);
-
-            }
-        });
-        calendar=Calendar.getInstance();
-        if (Build.VERSION.SDK_INT >= 23 ){
-            calendar.set(Calendar.HOUR_OF_DAY,picker.getHour());
-            calendar.set(Calendar.MINUTE,picker.getMinute());
-            calendar.set(Calendar.SECOND,0);
-            calendar.set(Calendar.MILLISECOND,0);
-        }
-        else{
-            calendar.set(Calendar.HOUR_OF_DAY,picker.getCurrentHour());
-            calendar.set(Calendar.MINUTE,picker.getCurrentMinute());
-            calendar.set(Calendar.SECOND,0);
-            calendar.set(Calendar.MILLISECOND,0);
-        }
-    }*/
-
-
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Beyond_SchoolChannel";
-            String description = "Channel for Setting Alarm";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel channel = new NotificationChannel("Beyond_school", name, importance);
-            channel.setDescription(description);
-            channel.enableVibration(true);
-
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-
-        }
-    }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
 
         finish();
+    }
+
+
+    private void checkCalenderPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  // M = 23
+            if (ContextCompat.checkSelfPermission(this, String.valueOf(new String[]{Manifest.permission.WRITE_CALENDAR,Manifest.permission.READ_CALENDAR})) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CALENDAR,Manifest.permission.READ_CALENDAR}, REQUEST_CALENDER_ACCESS);
+            }
+        }
+    }
+    
+    @SuppressLint("MissingSuperCall")
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if (requestCode == REQUEST_CALENDER_ACCESS) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+               // Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                checkCalenderPermission();
+            }
+        }
+
+    }
+
+    @SuppressLint("Range")
+    public void buttonClick(View view) throws ParseException {
+        Cursor cur = this.getContentResolver().query(CalendarContract.Calendars.CONTENT_URI,null, null, null, null);
+        try
+        {
+
+            PrefConfig.writeIdInPref(AlarmAtTime.this,binding.textInputLayoutTimer.getEditText().getText().toString(),getResources().getString(R.string.timer_time));
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy/MM/dd");
+            Date currDate=new Date();
+            String datetime= dateFormatter.format(currDate)+" "+binding.textInputLayoutTimer.getEditText().getText().toString().replace(" ","").split("-")[0];
+           String endTime= "2023/12/31 "+binding.textInputLayoutTimer.getEditText().getText().toString().replace(" ","").split("-")[1];
+          //  String endTime="2023/08/12 06:30";
+
+            Calendar startCal = Calendar.getInstance();
+            startCal.setTime(formatter.parse(datetime));
+            
+            Calendar endCal = Calendar.getInstance();
+            endCal.setTime(formatter.parse(endTime));
+            var eventdate = startCal.get(Calendar.YEAR)+"/"+startCal.get(Calendar.MONTH)+"/"+startCal.get(Calendar.DAY_OF_MONTH)+" "+startCal.get(Calendar.HOUR_OF_DAY)+":"+startCal.get(Calendar.MINUTE);
+            Log.e("event date",eventdate);
+            // provide CalendarContract.Calendars.CONTENT_URI to
+            // ContentResolver to query calendars
+
+            if (cur.moveToFirst())
+            {
+               long calendarID=cur.getLong(cur.getColumnIndex(CalendarContract.Calendars._ID));
+                ContentValues eventValues = new ContentValues();
+                eventValues.put(CalendarContract.Events.DTSTART, ((startCal.getTimeInMillis())));
+                eventValues.put(CalendarContract.Events.DTEND, ((endCal.getTimeInMillis())));
+                //eventValues.put(CalendarContract.Events.DURATION,  "+P10000H");
+                eventValues.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().toString());
+                eventValues.put (CalendarContract.Events.CALENDAR_ID, calendarID);
+                eventValues.put(CalendarContract.Events.TITLE, "Beyond School Reminder");
+              //  eventValues.put(CalendarContract.Events.RRULE,"FREQ=DAILY;COUNT=1;BYDAY=MO,TU");
+                eventValues.put(CalendarContract.Events.DESCRIPTION,"Its time to read!!");
+                eventValues.put(CalendarContract.Events.ALL_DAY,false);
+                eventValues.put(CalendarContract.Events.HAS_ALARM,true);
+
+                Uri eventUri = this.getContentResolver().insert(CalendarContract.Events.CONTENT_URI, eventValues);
+                var eventID = ContentUris.parseId(eventUri);
+
+
+                ContentValues reminder = new ContentValues();
+                reminder.put(CalendarContract.Reminders.EVENT_ID, eventID);
+                reminder.put(CalendarContract.Reminders.MINUTES, 5);
+
+                reminder.put(CalendarContract.Reminders.METHOD, CalendarContract.Reminders.METHOD_ALERT);
+                getContentResolver().insert(CalendarContract.Reminders.CONTENT_URI, reminder);
+                Toast.makeText(this, "Event Added", Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Log.i("Error_Events",e.getMessage());
+        }
+        finally
+        {
+            if (cur != null)
+            {
+                cur.close();
+            }
+        }
+
     }
 }
