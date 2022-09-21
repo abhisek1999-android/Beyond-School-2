ca9ed28e7262a79f8ae19516209fa1b84cbd5662
Ayaan <aiyuayaan22@gmail.com>
2022-09-21 12:58:33 +0530

    Changes :-  * Code refactor

--- app/src/main/java/com/maths/beyond_school_280720220930/english_activity/grammar/GrammarActivity.java
+++ app/src/main/java/com/maths/beyond_school_280720220930/english_activity/grammar/GrammarActivity.java
@@ -7,16 +7,12 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.maths.beyond_school_280720220930.LogActivity;
 import com.maths.beyond_school_280720220930.R;
 import com.maths.beyond_school_280720220930.databinding.ActivityGrammarBinding;
-import com.maths.beyond_school_280720220930.english_activity.grammar.fragments.IndentificationNoun.IdentifyingNounsFragment;
-import com.maths.beyond_school_280720220930.english_activity.grammar.fragments.Irregular_plural_noun.IrregularPluralNounFragment;
-import com.maths.beyond_school_280720220930.english_activity.grammar.fragments.common_and_proper_noun.CommonAndProperNounFragment;
 import com.maths.beyond_school_280720220930.utils.UtilityFunctions;
 
 public class GrammarActivity extends AppCompatActivity {
@@ -27,7 +23,6 @@ public class GrammarActivity extends AppCompatActivity {
     private Fragment currentFragment;
 
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,45 +30,9 @@ public class GrammarActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
-        currentFragment = getFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frame_layout_grammar, currentFragment)
-                .commit();
         setToolbar();
-        textButtonClick();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private Fragment getFragment() {
-        if (getIntent().getStringExtra(EXTRA_GRAMMAR_CATEGORY).equals(
-                getResources().getString(R.string.grammar_1)
-        )) {
-            return new IdentifyingNounsFragment();
-        } else if (getIntent().getStringExtra(EXTRA_GRAMMAR_CATEGORY).equals(
-                getResources().getString(R.string.grammar_2)
-        )) {
-            return new IrregularPluralNounFragment();
-        } else {
-            return new CommonAndProperNounFragment();
-        }
-    }
-
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private void onDestroyEngine() {
-        if (getIntent().getStringExtra(EXTRA_GRAMMAR_CATEGORY).equals(
-                getResources().getString(R.string.grammar_1)
-        )) {
-            ((IdentifyingNounsFragment) currentFragment).destroyEngine();
-        } else if (getIntent().getStringExtra(EXTRA_GRAMMAR_CATEGORY).equals(
-                getResources().getString(R.string.grammar_2)
-        )) {
-            ((IrregularPluralNounFragment) currentFragment).destroyEngine();
-        } else {
-            ((CommonAndProperNounFragment) currentFragment).destroyEngine();
-        }
-    }
 
     private void textButtonClick() {
         binding.giveTestButton.setOnClickListener(v -> {
@@ -83,9 +42,7 @@ public class GrammarActivity extends AppCompatActivity {
 
     private void setToolbar() {
         binding.toolBar.imageViewBack.setOnClickListener(v -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                onBackPressed();
-            }
+            onBackPressed();
         });
         binding.toolBar.titleText.setText(getResources().getString(R.string.grammar));
         binding.textViewCategory.setText(getIntent().
@@ -104,10 +61,4 @@ public class GrammarActivity extends AppCompatActivity {
         });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        onDestroyEngine();
-    }
 }
