312f26a79829ac31455d22f1c01756b3e2cf1289
Ayaan <aiyuayaan22@gmail.com>
2022-09-21 17:17:53 +0530

    Changes :-  * Grammar SST Done

--- app/src/main/java/com/maths/beyond_school_280720220930/adapters/Subject_Adapter.java
+++ app/src/main/java/com/maths/beyond_school_280720220930/adapters/Subject_Adapter.java
@@ -77,52 +77,49 @@ public class Subject_Adapter extends RecyclerView.Adapter<Subject_Adapter.Subjec
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
-//                if (subject_model.isIs_locked()) {
+                if (subject_model.isIs_locked()) {
 //                    needs to be intent values
-                if (res[0].toLowerCase(Locale.ROOT).equals("vocabulary")) {
-                    Intent intent = new Intent(context, EnglishActivity.class);
-                    Log.d("EnglishActivityXXX", "onClick: " + val.replace("Vocabulary", "").trim().toLowerCase() + " Intent : " + UtilityFunctions.getVocabularyCategoryFromAdapter(res[1].toLowerCase(Locale.ROOT)).name());
-                    intent.putExtra(Constants.EXTRA_VOCABULARY_DETAIL_CATEGORY, UtilityFunctions.getVocabularyCategoryFromAdapter(val.replace("Vocabulary", "").trim().toLowerCase(Locale.ROOT)).name());
-                    context.startActivity(intent);
-                    // Toast.makeText(context, res[1], Toast.LENGTH_SHORT).show();
-                } else if (res[0].toLowerCase(Locale.ROOT).equals("spelling")) {
+                    if (res[0].toLowerCase(Locale.ROOT).equals("vocabulary")) {
+                        Intent intent = new Intent(context, EnglishActivity.class);
+                        Log.d("EnglishActivityXXX", "onClick: " + val.replace("Vocabulary", "").trim().toLowerCase() + " Intent : " + UtilityFunctions.getVocabularyCategoryFromAdapter(res[1].toLowerCase(Locale.ROOT)).name());
+                        intent.putExtra(Constants.EXTRA_VOCABULARY_DETAIL_CATEGORY, UtilityFunctions.getVocabularyCategoryFromAdapter(val.replace("Vocabulary", "").trim().toLowerCase(Locale.ROOT)).name());
+                        context.startActivity(intent);
+                        // Toast.makeText(context, res[1], Toast.LENGTH_SHORT).show();
+                    } else if (res[0].toLowerCase(Locale.ROOT).equals("spelling")) {
 
-                    var intent = new Intent(context, SpellingActivity.class);
-                    intent.putExtra(EXTRA_SPELLING_DETAIL, val);
-                    context.startActivity(intent);
-                } else if (res[0].toLowerCase(Locale.ROOT).equals("grammar")) {
-                    Intent intent = new Intent(context, GrammarActivity.class);
-                    intent.putExtra(Constants.EXTRA_GRAMMAR_CATEGORY, val);
-                    context.startActivity(intent);
-                } else {
-
-
-                    if (!res[0].equals("Multiplication")) {
-                        multiplicationOption.multiplicationSelected();
-                        Intent intent = new Intent(context, LearningActivity.class);
-                        intent.putExtra("selected_sub", val);
-                        intent.putExtra("subject", res[res.length - 1].toLowerCase());
-                        intent.putExtra("max_digit", res[0]);
-                        intent.putExtra("video_url", subject_model.getUrl());
+                        var intent = new Intent(context, SpellingActivity.class);
+                        intent.putExtra(EXTRA_SPELLING_DETAIL, val);
+                        context.startActivity(intent);
+                    } else if (res[0].toLowerCase(Locale.ROOT).equals("grammar")) {
+                        Intent intent = new Intent(context, GrammarActivity.class);
+                        intent.putExtra(Constants.EXTRA_GRAMMAR_CATEGORY, val);
                         context.startActivity(intent);
                     } else {
-                        Intent intent = new Intent(context, MainActivity.class);
-                        intent.putExtra("selected_sub", val);
-                        intent.putExtra("subject", res[0].toLowerCase());
-                        intent.putExtra("max_digit", res[3]);
-                        intent.putExtra("video_url", subject_model.getUrl());
-                        context.startActivity(intent);
+
+
+                        if (!res[0].equals("Multiplication")) {
+                            multiplicationOption.multiplicationSelected();
+                            Intent intent = new Intent(context, LearningActivity.class);
+                            intent.putExtra("selected_sub", val);
+                            intent.putExtra("subject", res[res.length - 1].toLowerCase());
+                            intent.putExtra("max_digit", res[0]);
+                            intent.putExtra("video_url", subject_model.getUrl());
+                            context.startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(context, MainActivity.class);
+                            intent.putExtra("selected_sub", val);
+                            intent.putExtra("subject", res[0].toLowerCase());
+                            intent.putExtra("max_digit", res[3]);
+                            intent.putExtra("video_url", subject_model.getUrl());
+                            context.startActivity(intent);
+
+                        }
+
 
                     }
-
-
+                } else {
+                    UtilityFunctions.displayCustomDialog(context, "Chapter Locked", "Hey, Please complete previous level to unlock.");
                 }
-//                } else {
-//
-//                    UtilityFunctions.displayCustomDialog(context, "Chapter Locked", "Hey, Please complete previous level to unlock.");
-//
-//
-//                }
             }
         });
 
--- app/src/main/java/com/maths/beyond_school_280720220930/english_activity/grammar/GrammarActivity.java
+++ app/src/main/java/com/maths/beyond_school_280720220930/english_activity/grammar/GrammarActivity.java
@@ -2,13 +2,23 @@ package com.maths.beyond_school_280720220930.english_activity.grammar;
 
 import static com.maths.beyond_school_280720220930.utils.Constants.EXTRA_GRAMMAR_CATEGORY;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -19,10 +29,14 @@ import com.maths.beyond_school_280720220930.database.english.EnglishGradeDatabas
 import com.maths.beyond_school_280720220930.database.english.grammer.model.GrammarModel;
 import com.maths.beyond_school_280720220930.database.english.grammer.model.GrammarType;
 import com.maths.beyond_school_280720220930.databinding.ActivityGrammarBinding;
+import com.maths.beyond_school_280720220930.databinding.AnimSingleLayoutBinding;
+import com.maths.beyond_school_280720220930.dialogs.HintDialog;
 import com.maths.beyond_school_280720220930.english_activity.vocabulary.EnglishViewPager;
+import com.maths.beyond_school_280720220930.model.AnimData;
 import com.maths.beyond_school_280720220930.translation_engine.ConversionCallback;
 import com.maths.beyond_school_280720220930.translation_engine.TextToSpeechBuilder;
 import com.maths.beyond_school_280720220930.translation_engine.translator.TextToSpeckConverter;
+import com.maths.beyond_school_280720220930.utils.AnimationUtil;
 import com.maths.beyond_school_280720220930.utils.CollectionUtils;
 import com.maths.beyond_school_280720220930.utils.UtilityFunctions;
 
@@ -49,6 +63,15 @@ public class GrammarActivity extends AppCompatActivity {
     private final int REQUEST_INTRO = 44 * 2;
 
 
+    private TextToSpeckConverter ttsHelperAnim;
+    private int num = 0;
+    private List<AnimData> animEng;
+
+    private LinearLayout addAnimLayout, finalView;
+    private TextView descTextView, finalText;
+    private Animation slideLeftAnim, slideRightAnim, fadeIn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +81,21 @@ public class GrammarActivity extends AppCompatActivity {
 
         setToolbar();
         getDataFromIntent();
+        binding.hintButton.setOnClickListener(v -> {
+            try {
+                displayTutorialAnimation();
+                destroyEngine();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        binding.giveTestButton.setOnClickListener(v -> {
+            UtilityFunctions.simpleToast(context, "Test is not available & will be available soon");
+        });
+    }
+
+    private void initMediaPlayer() {
+        mediaPlayer = UtilityFunctions.playClapSound(this);
     }
 
     private void getDataFromIntent() {
@@ -80,6 +118,7 @@ public class GrammarActivity extends AppCompatActivity {
         fragmentList = mapToFragment(grammarModelList);
         var pagerAdapter = new EnglishViewPager(fragmentList, getSupportFragmentManager(), getLifecycle());
         binding.viewPagerIdentifyingNouns.setAdapter(pagerAdapter);
+        binding.viewPagerIdentifyingNouns.setUserInputEnabled(false);
         setIntro();
         setButton();
         setOptionButtonClick();
@@ -89,6 +128,7 @@ public class GrammarActivity extends AppCompatActivity {
         binding.playPause.setOnClickListener(v -> {
             if (binding.playPause.isChecked()) {
                 initTTS();
+                initMediaPlayer();
                 startSpeaking();
             } else
                 destroyEngine();
@@ -98,6 +138,7 @@ public class GrammarActivity extends AppCompatActivity {
 
     private void setIntro() {
         initTTS();
+        initMediaPlayer();
         playPauseAnimation(true);
         setToggleButtonChecked(true);
         helperTTS(UtilityFunctions.getIntroForGrammar(context, category)
@@ -155,6 +196,7 @@ public class GrammarActivity extends AppCompatActivity {
             Log.d("XXX", "checkAnswer: " + text + " " + currentAnswer);
             if (text.equals(currentAnswer)) {
                 playPauseAnimation(true);
+                mediaPlayer.start();
                 helperTTS(
                         UtilityFunctions.getCompliment(true)
                         , true
@@ -178,6 +220,16 @@ public class GrammarActivity extends AppCompatActivity {
                                 return;
                             }
                             if (canNavigate) {
+                                mediaPlayer.pause();
+                                if (binding.viewPagerIdentifyingNouns.getCurrentItem()
+                                        == grammarModelList.size() - 1) {
+                                    UtilityFunctions.runOnUiThread(() -> {
+                                        playPauseAnimation(false);
+                                        binding.playPause.setChecked(false);
+                                        displayCompleteDialog();
+                                    });
+                                    return;
+                                }
                                 binding.viewPagerIdentifyingNouns
                                         .setCurrentItem(binding
                                                 .viewPagerIdentifyingNouns.getCurrentItem() + 1);
@@ -302,6 +354,15 @@ public class GrammarActivity extends AppCompatActivity {
 
     }
 
+    @SuppressWarnings("deprecation")
+    static class TTSHelperAsyncTaskAnim extends AsyncTask<ConversionCallback, Void, TextToSpeckConverter> {
+        @Override
+        protected TextToSpeckConverter doInBackground(ConversionCallback... conversionCallbacks) {
+            return TextToSpeechBuilder.builder(conversionCallbacks[0]);
+        }
+    }
+
+
     private List<Fragment> mapToFragment(List<GrammarModel> grammarModels) {
         return CollectionUtils.mapWithIndex(grammarModels.stream(), (index, item) ->
                 new RowItemFragment(item, index + 1)).collect(Collectors.toList());
@@ -351,6 +412,129 @@ public class GrammarActivity extends AppCompatActivity {
             ttsHelper.destroy();
         }
     }
+
+    private void initTTSHelperAnim() throws ExecutionException, InterruptedException {
+        ttsHelperAnim = new TTSHelperAsyncTaskAnim().execute(new ConversionCallback[]{new ConversionCallback() {
+            @Override
+            public void onCompletion() {
+
+                Log.i("Index_Size", num + "");
+                if (num < animEng.size()) {
+
+                    UtilityFunctions.runOnUiThread(() -> {
+
+                        ttsHelperAnim.initialize(animEng.get(num).getDescription(), GrammarActivity.this);
+                        animHandel(animEng.get(num).getAnswer(), animEng.get(num).getDescription(), animEng.get(num).getOperation());
+                        num++;
+                    }, 500);
+                } else {
+                    num = 0;
+                }
+            }
+
+            @Override
+            public void onErrorOccurred(String errorMessage) {
+
+                ttsHelperAnim.destroy();
+            }
+        }}).get();
+    }
+
+
+    private void displayTutorialAnimation() throws ExecutionException, InterruptedException {
+
+
+        animEng = AnimationUtil.animGrammar(category);
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(GrammarActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.tutorial_anim, null);
+
+        num = 0;
+        ImageView closeButton = mView.findViewById(R.id.closeButton);
+        addAnimLayout = mView.findViewById(R.id.insert_point);
+        finalText = mView.findViewById(R.id.finalAns);
+        finalView = mView.findViewById(R.id.finalView);
+        descTextView = mView.findViewById(R.id.descTextView);
+
+        initTTSHelperAnim();
+
+        alert.setView(mView);
+        final AlertDialog alertDialog = alert.create();
+        alertDialog.setCancelable(true);
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        String initText = "Hi kids, Let us learn about " + category;
+
+        descTextView.setText(initText);
+        ttsHelperAnim.initialize(initText, GrammarActivity.this);
+        try {
+            alertDialog.show();
+        } catch (Exception e) {
+
+        }
+
+        closeButton.setOnClickListener(v -> {
+            alertDialog.dismiss();
+            ttsHelperAnim.stop();
+        });
+
+    }
+
+    private void displayCompleteDialog() {
+
+        HintDialog hintDialog = new HintDialog(this);
+        hintDialog.setCancelable(false);
+        hintDialog.setAlertTitle("Woohoo!!");
+        hintDialog.setAlertDesciption("Hey, you completed practice successfully !!\nNow you can proceed to take test.");
+
+        hintDialog.actionButton("START TEST");
+        hintDialog.actionButtonBackgroundColor(R.color.primary);
+        hintDialog.setOnActionListener(viewId -> {
+
+            switch (viewId.getId()) {
+
+                case R.id.closeButton:
+                    hintDialog.dismiss();
+                    break;
+                case R.id.buttonAction:
+                    UtilityFunctions.simpleToast(context, "Test is not available & will be available soon");
+                    break;
+            }
+        });
+
+
+        hintDialog.show();
+
+    }
+
+
+    private void animHandel(String answer, String description, String operation) {
+
+        slideLeftAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_left);
+        slideRightAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.anim_single_layout, null);
+        AnimSingleLayoutBinding binding = AnimSingleLayoutBinding.bind(view);
+
+        binding.description.setText(description);
+        binding.slNumView.setText((num + 1) + ".");
+
+        binding.mathLayout.setVisibility(View.GONE);
+        view.startAnimation(slideRightAnim);
+        ViewGroup main = (ViewGroup) addAnimLayout;
+        main.addView(view, num);
+
+
+//        if (num==animEng.size()-1){
+//
+//            finalView.setVisibility(View.VISIBLE);
+//            finalView.startAnimation(slideRightAnim);
+//            finalText.setText(answer.split("_")[2]);
+//        }
+
+
+    }
+
 
     interface ButtonClick {
         void onClick(String text);
