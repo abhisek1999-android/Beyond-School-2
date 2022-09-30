bddee6a57e9be2ca6e96832fb7533176cd22624a
Abhisek <abhiseal45@gmail.com>
2022-09-30 15:55:55 +0530

    code_merged_with_ayaan

--- app/src/main/java/com/maths/beyond_school_280720220930/TestActivity.java
+++ app/src/main/java/com/maths/beyond_school_280720220930/TestActivity.java
@@ -3,23 +3,28 @@ package com.maths.beyond_school_280720220930;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
-import com.maths.beyond_school_280720220930.extras.ReadText;
-import com.maths.beyond_school_280720220930.extras.RecognizeVoice;
+import com.maths.beyond_school_280720220930.SP.PrefConfig;
+import com.maths.beyond_school_280720220930.databinding.ActivityTestBinding;
+import com.maths.beyond_school_280720220930.retrofit.ApiClient;
+import com.maths.beyond_school_280720220930.retrofit.ApiInterface;
+import com.maths.beyond_school_280720220930.retrofit.Chapter;
+import com.maths.beyond_school_280720220930.retrofit.model.ResponseEnglish;
+import com.maths.beyond_school_280720220930.utils.GradeConverter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -29,9 +34,12 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        ActivityTestBinding binding = ActivityTestBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setUpRemoteConfig();
 
-
+    }
+    private void setUpRemoteConfig() {
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
                 .setMinimumFetchIntervalInSeconds(60)
@@ -40,7 +48,9 @@ public class TestActivity extends AppCompatActivity {
         mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
 
 
-
+        var value = PrefConfig.readIntInPref(this,
+                getResources().getString(R.string.KEY_VALUE_SAVE)
+                ,0);
 
         mFirebaseRemoteConfig.fetchAndActivate()
                 .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
@@ -49,20 +59,73 @@ public class TestActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             boolean updated = task.getResult();
 
-                            long val=mFirebaseRemoteConfig.getLong("data_updated_value");
+                            int val=(int)mFirebaseRemoteConfig.getLong("data_updated_value");
+                            if(value != val){
+                                PrefConfig.writeIntInPref(
+                                        TestActivity.this,
+                                        val,
+                                        getResources().getString(R.string.KEY_VALUE_SAVE)
+                                );
+                                getData();
+                            }
 
                             Log.d(TAG, "Config params updated: " + updated+", val:"+ val);
-                            Toast.makeText(TestActivity.this, "Fetch and activate succeeded  "+val,
-                                    Toast.LENGTH_SHORT).show();
-
-                        } else {
-                            Toast.makeText(TestActivity.this, "Fetch failed",
-                                    Toast.LENGTH_SHORT).show();
+                            return;
                         }
+                        Toast.makeText(TestActivity.this, "Fetch failed",
+                                Toast.LENGTH_SHORT).show();
 
                     }
                 });
+
+
     }
+
+
+    private void getData() {
+        Retrofit retrofit = ApiClient.getClient();
+        var api = retrofit.create(ApiInterface.class);
+        api.getTodos().enqueue(new retrofit2.Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseEnglish> call,
+                                   @NonNull Response<ResponseEnglish> response) {
+                var res = response.body();
+                if (res != null) {
+                    var english = res.getEnglish();
+                    convertData(english);
+                }else {
+                    Log.d(TAG, "onResponse: "+response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseEnglish> call, @NonNull Throwable t) {
+                Log.d(TAG, "onFailure: " + t.getLocalizedMessage());
+            }
+        });
+    }
+
+    private void convertData(List<Chapter> english) {
+        english.forEach(
+                chapter -> {
+                    var chapters = chapter.getChapters();
+                    var converter = new GradeConverter();
+                    var listOfGradeData = converter.mapToList(chapters); // Grade List similar to GradeDatabase
+                    listOfGradeData.forEach(gradeData -> {
+                        Log.d(TAG, "Subject: " + gradeData.subject);
+                        Log.d(TAG, "Chapter: " + gradeData.chapter);
+                        Log.d(TAG, "grade 1: " + gradeData.grade1);
+                        Log.d(TAG, "grade 2: " + gradeData.grade2);
+                        Log.d(TAG, "grade 3: " + gradeData.grade3);
+                        Log.d(TAG, "grade 4: " + gradeData.grade4);
+                        Log.d(TAG, "grade 5: " + gradeData.grade5);
+                        Log.d(TAG, "isCompleted: " + gradeData.is_completed);
+                        System.out.println();
+                    });
+                }
+        );
+    }
+
 
     public void buttonClick(View view) {
           finish();
