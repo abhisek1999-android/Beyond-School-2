8a7343dc771b5dddb8877d7d8a3c79713fc2307c
aiyu-ayaan <aiyuayaan22@gmail.com>
2022-11-03 15:08:23 +0530

    Changes :-  * Offline Caching .

--- app/src/main/java/com/maths/beyond_school_280720220930/retrofit/ApiClient.java
+++ app/src/main/java/com/maths/beyond_school_280720220930/retrofit/ApiClient.java
@@ -1,30 +1,42 @@
 package com.maths.beyond_school_280720220930.retrofit;
 
-import android.util.Log;
+import static com.facebook.FacebookSdk.getCacheDir;
 
-import java.util.List;
 import java.util.Objects;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
+    private static final long cacheSize = 5 * 1024 * 1024; // 5 MB
+
     private static final String BASE_URL = "https://shashuec.github.io/beyondschool_data/data/";
     private static Retrofit retrofit = null;
+
+    private static final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .cache(new Cache(Objects.requireNonNull(getCacheDir()), cacheSize))
+            .addInterceptor(chain -> {
+                Request request = chain.request();
+                request = request.newBuilder().header("Cache-Control", "public, max-age=" + 60 * 60 * 24).build();
+                return chain.proceed(request);
+            })
+            .build();
 
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
         return retrofit;
     }
+
 }
 
 // ----------------------------------------Getting data using retrofit-------------------------
